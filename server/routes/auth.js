d06c18718baadad5a4758ee7d8bfc06d7fa1e564
PRATHAM GUPTA <91843874+PRO-11@users.noreply.github.com>
2023-05-06 00:44:52 +0530

    Vercel config

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,19 +1,10 @@
 import  express  from "express";
 import { googleAuth, signin, signup} from "../api/auth.js";
-import User from "../models/User.js"
-import bcrypt from "bcryptjs"
-import jwt from "jsonwebtoken"
+
 const router =express.Router();
 
 //Signup
-router.post("/signup",async(req,res)=>{
-    const salt=bcrypt.genSaltSync(10)
-    const hash=bcrypt.hashSync(req.body.password,salt)
-    const newUser=new User({...req.body,password:hash})
-
-    await newUser.save()
-    res.status(200).json({msg:"User has been created" })
-})
+router.post("/signup",signup)
 
 //Signin
 router.post("/signin",signin)
